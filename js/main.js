330c88b67a01afd73b234ff2704e54308f2aa8d2
piaocreative <johnpiao171019@gmail.com>
2021-10-27 23:17:40 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.logos').slick({
+        infinite: false,
+        slidesToShow: 7,
+        slidesToScroll: 7,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    infinite: false,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    infinite: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            }
+        ]
+    });
+});
